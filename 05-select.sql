e6d062d8ac3864ffc1e5645818dcebcc7e42528b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-28 22:08:50 -0300

    part 2

--- 05-select.sql
+++ 05-select.sql
@@ -26,8 +26,33 @@ select * from cursos
 where carga > 35 and totaulas < 30    /* Use operadores logicos */
 order by nome;
 
-/*  */
+/* Selecione por nome */
+select * from cursos
+where nome = 'PHP';
 
+/* Operador LIKE */
+select * from cursos
+where nome like 'P%';   /* Mostra coisas parecidas */
+                       /* A% = começan com A, %A= termina com A, %A% = todos os registros com A em algum lugar */
+                       /* Coloque not like para que não tenha o caracter */
+                       /* p_p%    _ = tenha uma letra ai*/
+
+/* Distinguindo */
+select distinct nacionalidade from gafanhotos;    /* lista sem repetir o mesmo resultado */
+
+/* funções de agregação */
+select count(*) from cursos;    /* Mostra a quantidade numerica */
+select count(nome) from cursos;
+
+select max(carga) from cursos   /* Mostra o maior numero */
+order by carga; 
+
+select min(carga) from cursos   /* Mostra o menor numero numero */
+order by carga; 
+
+select sum(totaulas) from cursos;   /* Soma todos os numeros */
+
+select avg(totaulas) from cursos;   /* Tira a media */
 
 
 
